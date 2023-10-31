0300f0093ce79e199e89f543a57844552d4f285a
SelvasundaramKSelva <149467367+SelvasundaramKSelva@users.noreply.github.com>
2023-10-31 07:54:56 +0000

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,44 @@
+let a=5;
+let b=6;
+let c=a/b;
+ d=(a+b)*c;
+console.log("The answer is" +" " + d)
+
+// code using logical operators
+let x=1,y=4,z=3;
+let n=x<y && y<z;
+i=!n;
+console.log(n,i)
+
+
+const items=["pencil","pen", "rubber"];    //code using array
+console.log(items.length,items[2] )
+
+
+const fruits =['apple','orange','banana']  // code using push command
+console.log(fruits,fruits.push('lemon'))
+
+//code using if else statement
+const day=24;
+if(day<30){
+  month='february';
+}
+else {
+  month='other months'
+}
+console.log(month)
+
+//code using switch statement
+let e=1;
+switch(e){
+case 1:
+e='one';
+break;
+case 2:
+e="two";
+break;
+case 3:
+e="three"
+break;
+}
+console.log(e)
---
+++ basics2.js
@@ -0,0 +1,18 @@
+const a=5;                //for loop
+for (let i=2;i<=a;i++)
+console.log(i)
+
+
+
+                      //while loop
+let j=3;
+while(j<5){
+    console.log(j)
+    j++;
+}
+
+let n=3;              //do while loop
+do{
+    console.log(n)
+    n++;
+}while(n<=2);
